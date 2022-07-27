ef67b483290b351e7402fcd77666d6214f83a16f
MarceloMottaa <105554617+MarceloMottaa@users.noreply.github.com>
2022-07-27 17:09:49 -0300

    Add files via upload

---
+++ Ciclo/js/main.js
@@ -0,0 +1,15 @@
+
+ /* EJEMPLO CON NOMBRES */
+
+/* let name = prompt("Ingrese su nombre:")
+let apellido = prompt("Ingese su apellido")
+let edad = prompt ("Ingresar su edad:") 
+
+alert("Bienvenid@ " + name + " " + apellido + ", tu edad es " +edad ) */
+
+/* EJEMPLO CON NÚMEROS */
+let num = parseInt(prompt("Ingrese un número"))
+
+for(let i=0; i<=10; i++){
+    console.log(num + i)
+} 
